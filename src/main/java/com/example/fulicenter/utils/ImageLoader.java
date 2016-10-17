5cb1463a88aa3700ab51050ea1644faa0911dd4a
Matao <1229450981@qq.com>
2016-10-17 22:40:15 +0800

    添加显示图片的工具类

--- src/main/java/com/example/fulicenter/utils/ImageLoader.java
+++ src/main/java/com/example/fulicenter/utils/ImageLoader.java
@@ -9,17 +9,20 @@ import android.util.Log;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
+import com.example.fulicenter.I;
+import com.example.fulicenter.R;
+
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 
-import cn.ucai.fulicenter.I;
-import cn.ucai.fulicenter.R;
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+
+
 
 /**
  * Created by yao on 2016/5/18.
