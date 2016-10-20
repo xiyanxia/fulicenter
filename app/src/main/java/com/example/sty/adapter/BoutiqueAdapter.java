ea027527300a850ff6d0aea4a3ba32cedc10cf9a
Matao <1229450981@qq.com>
2016-10-21 02:42:09 +0800

    分类的二级界面的数据显示和数据上架时间和排序

---
+++ app/src/main/java/com/example/sty/adapter/BoutiqueAdapter.java
@@ -0,0 +1,91 @@
+package com.example.sty.adapter;
+
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView.Adapter;
+import android.support.v7.widget.RecyclerView.ViewHolder;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.example.winston.myapplication.R;
+import com.example.winston.myapplication.bean.BoutiqueBean;
+import com.example.winston.myapplication.utils.ImageLoader;
+import com.example.winston.myapplication.utils.MFGT;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+/**
+ * Created by Winston on 2016/10/19.
+ */
+
+public class BoutiqueAdapter extends Adapter<BoutiqueAdapter.BoutiqueViewHolder> {
+    Context mContext;
+    ArrayList<BoutiqueBean> mList;
+
+    public BoutiqueAdapter(Context context, ArrayList<BoutiqueBean> list) {
+        mContext = context;
+        mList = new ArrayList<>();
+        mList.addAll(list);
+    }
+
+    @Override
+    public BoutiqueViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        BoutiqueViewHolder holder = new BoutiqueViewHolder(LayoutInflater.from(mContext)
+                .inflate(R.layout.item_boutique, parent, false));
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(BoutiqueViewHolder holder, int position) {
+        BoutiqueBean boutiqueBean = mList.get(position);
+        ImageLoader.downloadImg(mContext,holder.mIvBoutiqueImg,boutiqueBean.getImageurl());
+        holder.mTvBoutiqueTitle.setText(boutiqueBean.getTitle());
+        holder.mTvBoutiqueName.setText(boutiqueBean.getName());
+        holder.mTvBoutiqueDescription.setText(boutiqueBean.getDescription());
+        holder.mLayoutBoutiqueItem.setTag(boutiqueBean);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mList != null ? mList.size(): 0;
+    }
+
+    public void initData(ArrayList<BoutiqueBean> list) {
+        if(mList!=null){
+            mList.clear();
+        }
+        mList.addAll(list);
+        notifyDataSetChanged();
+    }
+
+    class BoutiqueViewHolder extends ViewHolder {
+        @BindView(R.id.ivBoutiqueImg)
+        ImageView mIvBoutiqueImg;
+        @BindView(R.id.tvBoutiqueTitle)
+        TextView mTvBoutiqueTitle;
+        @BindView(R.id.tvBoutiqueName)
+        TextView mTvBoutiqueName;
+        @BindView(R.id.tvBoutiqueDescription)
+        TextView mTvBoutiqueDescription;
+        @BindView(R.id.layout_boutique_item)
+        RelativeLayout mLayoutBoutiqueItem;
+
+        BoutiqueViewHolder(View view) {
+            super(view);
+            ButterKnife.bind(this, view);
+        }
+        @OnClick(R.id.layout_boutique_item)
+        public void onBoutiqueClick(){
+            BoutiqueBean bean = (BoutiqueBean) mLayoutBoutiqueItem.getTag();
+            MFGT.gotoBoutiqueChildActivity(mContext,bean);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/sty/adapter/GoodsAdapter.java
@@ -0,0 +1,163 @@
+package com.example.sty.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView.Adapter;
+import android.support.v7.widget.RecyclerView.ViewHolder;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.example.winston.myapplication.I;
+import com.example.winston.myapplication.R;
+import com.example.winston.myapplication.bean.NewGoodsBean;
+import com.example.winston.myapplication.utils.ImageLoader;
+import com.example.winston.myapplication.utils.MFGT;
+import com.example.winston.myapplication.view.FooterViewHolder;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+/**
+ * Created by Winston on 2016/10/17.
+ */
+
+public class GoodsAdapter extends Adapter {
+    Context mContext;
+    List<NewGoodsBean> mList;
+    boolean isMore;
+    int sortBy=I.SORT_BY_ADDTIME_DESC;
+
+    public GoodsAdapter(Context context, List<NewGoodsBean> list) {
+        mContext = context;
+        mList = new ArrayList<>();
+        mList.addAll(list);
+    }
+
+    public void setSortBy(int sortBy) {
+        this.sortBy = sortBy;
+        sortBy();
+        notifyDataSetChanged();
+    }
+
+    public boolean isMore() {
+        return isMore;
+    }
+
+    public void setMore(boolean more) {
+        isMore = more;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        ViewHolder holder = null;
+        if (viewType == I.TYPE_FOOTER) {
+            holder = new FooterViewHolder(View.inflate(mContext, R.layout.item_footer, null));
+        } else {
+            holder = new GoodsViewHolder(View.inflate(mContext, R.layout.item_goods, null));
+        }
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        if(getItemViewType(position)==I.TYPE_FOOTER){
+            FooterViewHolder vh = (FooterViewHolder) holder;
+            vh.mTvFooter.setText(getFootString());
+        }else{
+            GoodsViewHolder vh = (GoodsViewHolder) holder;
+            NewGoodsBean goods = mList.get(position);
+            ImageLoader.downloadImg(mContext,vh.mIvGoodsThumb,goods.getGoodsThumb());
+            vh.mTvGoodsName.setText(goods.getGoodsName());
+            vh.mTvGoodsPrice.setText(goods.getCurrencyPrice());
+            vh.mLayoutGoods.setTag(goods.getGoodsId());
+        }
+    }
+
+    private int getFootString() {
+        return isMore?R.string.load_more:R.string.no_more;
+    }
+
+    @Override
+    public int getItemCount() {
+        return mList != null ? mList.size() + 1 : 1;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (position == getItemCount() - 1) {
+            return I.TYPE_FOOTER;
+        }
+        return I.TYPE_ITEM;
+    }
+
+    public void initData(ArrayList<NewGoodsBean> list) {
+        if(mList!=null){
+            mList.clear();
+        }
+        mList.addAll(list);
+        notifyDataSetChanged();
+    }
+
+    public void addData(ArrayList<NewGoodsBean> list) {
+        mList.addAll(list);
+        notifyDataSetChanged();
+    }
+
+    class GoodsViewHolder extends ViewHolder{
+        @BindView(R.id.ivGoodsThumb)
+        ImageView mIvGoodsThumb;
+        @BindView(R.id.tvGoodsName)
+        TextView mTvGoodsName;
+        @BindView(R.id.tvGoodsPrice)
+        TextView mTvGoodsPrice;
+        @BindView(R.id.layout_goods)
+        LinearLayout mLayoutGoods;
+
+        GoodsViewHolder(View view) {
+            super(view);
+            ButterKnife.bind(this, view);
+        }
+        @OnClick(R.id.layout_goods)
+        public void onGoodsItemClick(){
+            int goodsId = (int) mLayoutGoods.getTag();
+            MFGT.gotoGoodsDetailsActivity(mContext,goodsId);
+        }
+    }
+
+    private void sortBy(){
+        Collections.sort(mList, new Comparator<NewGoodsBean>() {
+            @Override
+            public int compare(NewGoodsBean left, NewGoodsBean right) {
+                int result=0;
+                switch (sortBy){
+                    case I.SORT_BY_ADDTIME_ASC:
+                        result= (int) (Long.valueOf(left.getAddTime())-Long.valueOf(right.getAddTime()));
+                        break;
+                    case I.SORT_BY_ADDTIME_DESC:
+                        result= (int) (Long.valueOf(right.getAddTime())-Long.valueOf(left.getAddTime()));
+                        break;
+                    case I.SORT_BY_PRICE_ASC:
+                        result = getPrice(left.getCurrencyPrice())-getPrice(right.getCurrencyPrice());
+                        break;
+                    case I.SORT_BY_PRICE_DESC:
+                        result = getPrice(right.getCurrencyPrice())-getPrice(left.getCurrencyPrice());
+                        break;
+                }
+                return result;
+            }
+            private int getPrice(String price){
+                price = price.substring(price.indexOf("￥")+1);
+                return Integer.valueOf(price);
+            }
+        });
+    }
+}
