03c2a913e5cdee40600ba52e90b0a2f36734b97b
Matao <1229450981@qq.com>
2016-10-21 02:36:28 +0800

    分类的adapter的数据填充

---
+++ app/src/main/java/com/example/sty/activity/CategoryChildActivity.java
@@ -0,0 +1,214 @@
+package com.example.sty.activity;
+
+
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.winston.myapplication.I;
+import com.example.winston.myapplication.R;
+import com.example.winston.myapplication.adapter.GoodsAdapter;
+import com.example.winston.myapplication.bean.CategoryChildBean;
+import com.example.winston.myapplication.bean.NewGoodsBean;
+import com.example.winston.myapplication.net.NetDao;
+import com.example.winston.myapplication.net.OkHttpUtils;
+import com.example.winston.myapplication.utils.CommonUtils;
+import com.example.winston.myapplication.utils.ConvertUtils;
+import com.example.winston.myapplication.utils.L;
+import com.example.winston.myapplication.utils.MFGT;
+import com.example.winston.myapplication.view.CatChildFilterButton;
+import com.example.winston.myapplication.view.SpaceItemDecoration;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+/**
+ * Created by Winston on 2016/10/20.
+ */
+
+public class CategoryChildActivity extends BaseActivity {
+    @BindView(R.id.tv_refresh)
+    TextView mTvRefresh;
+    @BindView(R.id.rv)
+    RecyclerView rv;
+    @BindView(R.id.srl)
+    SwipeRefreshLayout mSrl;
+    CategoryChildActivity mContext;
+    GoodsAdapter mAdapter;
+    ArrayList<NewGoodsBean> mList;
+    int pageId = 1;
+    GridLayoutManager glm;
+    int catId;
+    @BindView(R.id.btn_sort_price)
+    Button btnSortPrice;
+    @BindView(R.id.btn_sort_addtime)
+    Button btnSortAddtime;
+    boolean addTimeAsc = false;
+    boolean priceAsc = false;
+    int sortBy = I.SORT_BY_ADDTIME_DESC;
+    @BindView(R.id.btnCatChildFilter)
+    CatChildFilterButton mBtnCatChildFilter;
+    String groupName;
+    ArrayList<CategoryChildBean> mChildList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_category_child);
+        ButterKnife.bind(this);
+        mContext = this;
+        mList = new ArrayList<>();
+        mAdapter = new GoodsAdapter(mContext, mList);
+        catId = getIntent().getIntExtra(I.CategoryChild.CAT_ID, 0);
+        if (catId == 0) {
+            finish();
+        }
+        groupName = getIntent().getStringExtra(I.CategoryGroup.NAME);
+        mChildList = (ArrayList<CategoryChildBean>) getIntent().getSerializableExtra(I.CategoryChild.ID);
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void initView() {
+        mSrl.setColorSchemeColors(
+                getResources().getColor(R.color.google_blue),
+                getResources().getColor(R.color.google_green),
+                getResources().getColor(R.color.google_red),
+                getResources().getColor(R.color.google_yellow)
+        );
+        glm = new GridLayoutManager(mContext, I.COLUM_NUM);
+        rv.setLayoutManager(glm);
+        rv.setHasFixedSize(true);
+        rv.setAdapter(mAdapter);
+        rv.addItemDecoration(new SpaceItemDecoration(12));
+        mBtnCatChildFilter.setText(groupName);
+    }
+
+    @Override
+    protected void setListener() {
+        setPullUpListener();
+        setPullDownListener();
+    }
+
+    private void setPullDownListener() {
+        mSrl.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                mSrl.setRefreshing(true);
+                mTvRefresh.setVisibility(View.VISIBLE);
+                pageId = 1;
+                downloadCategoryGoods(I.ACTION_PULL_DOWN);
+            }
+        });
+    }
+
+    private void downloadCategoryGoods(final int action) {
+        NetDao.downloadCategoryGoods(mContext, catId, pageId, new OkHttpUtils.OnCompleteListener<NewGoodsBean[]>() {
+            @Override
+            public void onSuccess(NewGoodsBean[] result) {
+                mSrl.setRefreshing(false);
+                mTvRefresh.setVisibility(View.GONE);
+                mAdapter.setMore(true);
+                L.e("result=" + result);
+                if (result != null && result.length > 0) {
+                    ArrayList<NewGoodsBean> list = ConvertUtils.array2List(result);
+                    if (action == I.ACTION_DOWNLOAD || action == I.ACTION_PULL_DOWN) {
+                        mAdapter.initData(list);
+                    } else {
+                        mAdapter.addData(list);
+                    }
+                    if (list.size() < I.PAGE_SIZE_DEFAULT) {
+                        mAdapter.setMore(false);
+                    }
+                } else {
+                    mAdapter.setMore(false);
+                }
+            }
+
+            @Override
+            public void onError(String error) {
+                mSrl.setRefreshing(false);
+                mTvRefresh.setVisibility(View.GONE);
+                mAdapter.setMore(false);
+                CommonUtils.showShortToast(error);
+                L.e("error:" + error);
+            }
+        });
+    }
+
+    private void setPullUpListener() {
+        rv.setOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                int lastPosition = glm.findLastVisibleItemPosition();
+                if (newState == RecyclerView.SCROLL_STATE_IDLE
+                        && lastPosition == mAdapter.getItemCount() - 1
+                        && mAdapter.isMore()) {
+                    pageId++;
+                    downloadCategoryGoods(I.ACTION_PULL_UP);
+                }
+            }
+
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                int firstPosition = glm.findFirstVisibleItemPosition();
+                mSrl.setEnabled(firstPosition == 0);
+            }
+        });
+    }
+
+    @Override
+    protected void initData() {
+        downloadCategoryGoods(I.ACTION_DOWNLOAD);
+        mBtnCatChildFilter.setOnCatFilterClickListener(groupName,mChildList);
+    }
+
+    @OnClick(R.id.backClickArea)
+    public void onClick() {
+        MFGT.finish(this);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
+    @OnClick({R.id.btn_sort_price, R.id.btn_sort_addtime})
+    public void onClick(View view) {
+        Drawable right;
+        switch (view.getId()) {
+            case R.id.btn_sort_price:
+                if (priceAsc) {
+                    sortBy = I.SORT_BY_PRICE_ASC;
+                    right = getResources().getDrawable(R.mipmap.arrow_order_up);
+                } else {
+                    sortBy = I.SORT_BY_PRICE_DESC;
+                    right = getResources().getDrawable(R.mipmap.arrow_order_down);
+                }
+                right.setBounds(0, 0, right.getIntrinsicWidth(), right.getIntrinsicHeight());
+                btnSortPrice.setCompoundDrawablesRelativeWithIntrinsicBounds(null, null, right, null);
+                priceAsc = !priceAsc;
+                break;
+            case R.id.btn_sort_addtime:
+                if (addTimeAsc) {
+                    sortBy = I.SORT_BY_ADDTIME_ASC;
+                    right = getResources().getDrawable(R.mipmap.arrow_order_up);
+                } else {
+                    sortBy = I.SORT_BY_ADDTIME_DESC;
+                    right = getResources().getDrawable(R.mipmap.arrow_order_down);
+                }
+                right.setBounds(0, 0, right.getIntrinsicWidth(), right.getIntrinsicHeight());
+                btnSortAddtime.setCompoundDrawablesRelativeWithIntrinsicBounds(null, null, right, null);
+                addTimeAsc = !addTimeAsc;
+                break;
+        }
+        mAdapter.setSortBy(sortBy);
+    }
+}
---
+++ app/src/main/java/com/example/sty/adapter/CategoryAdapter.java
@@ -0,0 +1,165 @@
+package com.example.sty.adapter;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.example.winston.myapplication.R;
+import com.example.winston.myapplication.bean.CategoryChildBean;
+import com.example.winston.myapplication.bean.CategoryGroupBean;
+import com.example.winston.myapplication.utils.ImageLoader;
+import com.example.winston.myapplication.utils.MFGT;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+
+/**
+ * Created by Winston on 2016/10/20.
+ */
+
+public class CategoryAdapter extends BaseExpandableListAdapter {
+    Context mContext;
+    ArrayList<CategoryGroupBean> mGroupList;
+    ArrayList<ArrayList<CategoryChildBean>> mChildList;
+
+    public CategoryAdapter(Context Context, ArrayList<CategoryGroupBean> GroupList,
+                           ArrayList<ArrayList<CategoryChildBean>> ChildList) {
+        mContext = Context;
+        mGroupList = new ArrayList<>();
+        mGroupList.addAll(GroupList);
+        mChildList = new ArrayList<>();
+        mChildList.addAll(ChildList);
+    }
+
+    @Override
+    public int getGroupCount() {
+        return mGroupList != null ? mGroupList.size() : 0;
+    }
+
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        return mChildList != null && mChildList.get(groupPosition) != null ?
+                mChildList.get(groupPosition).size() : 0;
+    }
+
+    @Override
+    public CategoryGroupBean getGroup(int groupPosition) {
+        return mGroupList != null ? mGroupList.get(groupPosition) : null;
+    }
+
+    @Override
+    public CategoryChildBean getChild(int groupPosition, int childPosition) {
+        return mChildList != null && mChildList.get(groupPosition) != null ?
+                mChildList.get(groupPosition).get(childPosition) : null;
+    }
+
+    @Override
+    public long getGroupId(int groupPosition) {
+        return 0;
+    }
+
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return 0;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpand, View view, ViewGroup ViewGroup) {
+        GroupViewHolder holder;
+        if (view == null) {
+            view = View.inflate(mContext, R.layout.item_categroy_group, null);
+            holder = new GroupViewHolder(view);
+            view.setTag(holder);
+        } else {
+            view.getTag();
+            holder = (GroupViewHolder) view.getTag();
+        }
+        CategoryGroupBean group = getGroup(groupPosition);
+        if (group != null) {
+            ImageLoader.downloadImg(mContext, holder.mivGroupThumb, group.getImageUrl());
+            holder.mtvGroupName.setText(group.getName());
+            holder.mivIndicator.setImageResource(isExpand ? R.mipmap.expand_off : R.mipmap.expand_on);
+        }
+        return view;
+    }
+
+    @Override
+    public View getChildView(final int groupPosition, int childPosition, boolean isLastChild, View view, ViewGroup parent) {
+        ChildViewHolder holder;
+        if (view == null) {
+            view = View.inflate(mContext, R.layout.item_category_child, null);
+            holder = new ChildViewHolder(view);
+            view.setTag(holder);
+        } else {
+            holder = (ChildViewHolder) view.getTag();
+        }
+        final CategoryChildBean child = getChild(groupPosition, childPosition);
+        if (child != null) {
+            ImageLoader.downloadImg(mContext, holder.mivCategoryChildThumb, child.getImageUrl());
+            holder.mtvCategoryChildName.setText(child.getName());
+            holder.mlayoutCategoryChild.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    ArrayList<CategoryChildBean> list = mChildList.get(groupPosition);
+                    String groupName = mGroupList.get(groupPosition).getName();
+                    MFGT.gotoCategoryChildActivity(mContext,child.getId(),groupName,list);
+                }
+            });
+        }
+        return view;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return false;
+    }
+
+    public void initData(ArrayList<CategoryGroupBean> mGroupList, ArrayList<ArrayList<CategoryChildBean>> mChildList) {
+        if (mGroupList != null) {
+            mGroupList.clear();
+        }
+        mGroupList.addAll(mGroupList);
+        if (mChildList != null) {
+            mChildList.addAll(mChildList);
+            notifyDataSetChanged();
+        }
+    }
+
+    class GroupViewHolder {
+        @BindView(R.id.iv_group_thumb)
+        ImageView mivGroupThumb;
+        @BindView(R.id.tv_group_name)
+        TextView mtvGroupName;
+        @BindView(R.id.iv_indicator)
+        ImageView mivIndicator;
+
+        GroupViewHolder(View view) {
+            ButterKnife.bind(this, view);
+        }
+    }
+
+    class ChildViewHolder {
+        @BindView(R.id.iv_category_child_thumb)
+        ImageView mivCategoryChildThumb;
+        @BindView(R.id.tv_category_child_name)
+        TextView mtvCategoryChildName;
+        @BindView(R.id.layout_category_child)
+        LinearLayout mlayoutCategoryChild;
+
+        ChildViewHolder(View view) {
+            ButterKnife.bind(this, view);
+        }
+    }
+}
