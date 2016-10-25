bd6c17a5c4be8d59cfb001c3d7cbd4b35a092a02
Matao <1229450981@qq.com>
2016-10-25 20:02:18 +0800

    个人中心添加orderlist

--- app/src/main/java/com/example/sty/fragment/PersonalCenterFragment.java
+++ app/src/main/java/com/example/sty/fragment/PersonalCenterFragment.java
@@ -5,7 +5,9 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
 import com.example.sty.FuLiCenterApplication;
@@ -14,6 +16,9 @@ import com.example.sty.activity.MainActivity;
 import com.example.sty.bean.User;
 import com.example.sty.utils.ImageLoader;
 import com.example.sty.utils.MFGT;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -32,6 +37,9 @@ public class PersonalCenterFragment extends BaseFragment {
 
     MainActivity mContext;
 
+    @BindView(R.id.center_user_order_lis)
+    GridView mCenterUserOrderLis;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -44,7 +52,7 @@ public class PersonalCenterFragment extends BaseFragment {
 
     @Override
     protected void initView() {
-
+        initOrderList();
     }
 
     @Override
@@ -64,6 +72,31 @@ public class PersonalCenterFragment extends BaseFragment {
     }
 
     @OnClick(R.id.tv_center_settings)
-    public void onClick() {
+        public void onClick() {
+//        @OnClick({R.id.tv_center_settings,R.id.center_user_info})
+//        public void gotoSettings() {
+//                   MFGT.gotoSettings(mContext);
+//        }
     }
+    private void initOrderList() {
+                ArrayList<HashMap<String, Object>> data = new ArrayList<HashMap<String, Object>>();
+                HashMap<String, Object> order1 = new HashMap<String, Object>();
+                order1.put("order", R.drawable.order_list1);
+                data.add(order1);
+                HashMap<String, Object> order2 = new HashMap<String, Object>();
+                order2.put("order", R.drawable.order_list2);
+                data.add(order2);
+                HashMap<String, Object> order3 = new HashMap<String, Object>();
+                order3.put("order", R.drawable.order_list3);
+                data.add(order3);
+                HashMap<String, Object> order4 = new HashMap<String, Object>();
+                order4.put("order", R.drawable.order_list4);
+                data.add(order4);
+                HashMap<String, Object> order5 = new HashMap<String, Object>();
+               order5.put("order", R.drawable.order_list5);
+               data.add(order5);
+             SimpleAdapter adapter = new SimpleAdapter(mContext, data, R.layout.simple_adapter,
+                new String[]{"order"}, new int[]{R.id.iv_order});
+             mCenterUserOrderLis.setAdapter(adapter);
+        }
 }
